9b016cbf78f0611eeaab81af4a5a97abdd801706
artemnutvob <artemwest2018@gmail.com>
2025-11-10 22:09:25 +0100

    complete push_back + gitignore file

--- my_deque.cpp
+++ my_deque.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #define SIZE_OF_BUCKET  32
 #define DEFAULT_SIZE  2
+#define SIZE_SCALE  2
+
 
 struct Index
 {
@@ -15,8 +17,8 @@ class my_deque {
     T** arr_ = nullptr;
     Index front_;
     Index back_;
-    size_t size_;
-    size_t arr_size_;
+    size_t size_ = 0;
+    size_t arr_size_ = 0;
 public:
     using value_type = T;
 
@@ -28,6 +30,7 @@ public:
 
 template <typename T>
 void my_deque<T>::push_back(const T& val) {
+    ++size_;
     if(!arr_) {
         arr_ = new T*[DEFAULT_SIZE];
         arr_size_ = DEFAULT_SIZE;
@@ -37,9 +40,34 @@ void my_deque<T>::push_back(const T& val) {
             arr_[i] = reinterpret_cast<T*>(new char[SIZE_OF_BUCKET*sizeof(T)]);
         }
             
-        new(arr_[back_.i]) T(val); // check for throw next time
-
+        new(arr_[back_.i]) T(val); // check if this throw and how it. next time !!!
+        return;
     }
+    if(++back_.j == SIZE_OF_BUCKET ) {
+        back_.j = 0;
+        if(++back_.i == arr_size_){
+            // reallocate
+            T** newarr = new T*[arr_size_* SIZE_SCALE];
+            size_t half_size = arr_size_/2;
+            front_.i += half_size;
+            back_.i += half_size;
+            size_t index = 0;
+            for(;index < half_size;++index) {
+                newarr[index] = reinterpret_cast<T*>(new char[SIZE_OF_BUCKET*sizeof(T)]);
+            }
+            for(int i = 0;index < arr_size_ + half_size;++index, ++i) {
+                newarr[index] = arr_[i]; 
+            }
+            arr_size_ *= SIZE_SCALE;
+            for(;index < arr_size_ ;++index) {
+                newarr[index] = reinterpret_cast<T*>(new char[SIZE_OF_BUCKET*sizeof(T)]);
+            }
+            delete[] reinterpret_cast<char*>(arr_);
+            arr_ = newarr; 
+        }
+    }
+    new(arr_[back_.i]) T(val);
+    
 }
 
 template <typename T>
@@ -60,7 +88,8 @@ my_deque<T>::~my_deque() {
 }
 int main() {
     my_deque<int> d;
-    d.push_back(3);
-
+    for(int i = 0; i < 1000;++i) {
+        d.push_back(15);
+    }
     
 }
