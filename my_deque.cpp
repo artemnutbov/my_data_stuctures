dc51472ae57b685e0967af6db2c2164b239b0395
artemnutvob <artemwest2018@gmail.com>
2025-11-10 21:25:08 +0100

    part of push_back and destructor

--- my_deque.cpp
+++ my_deque.cpp
@@ -1,26 +1,66 @@
 #include <iostream>
-#define SIZE_OF_BUCKET = 32;
-
+#define SIZE_OF_BUCKET  32
+#define DEFAULT_SIZE  2
 
 struct Index
 {
-    int i;
-    int j;
+    size_t i;
+    size_t j;
     Index() = default;
-    Index(int i,int j):i(i),j(j) {}
+    Index(size_t i,size_t j):i(i),j(j) {}
 };
 
 template <typename T> 
 class my_deque {
-    T** _arr = nullptr;
-    Index _front;
-    Index _back;
-    size_t _size;
+    T** arr_ = nullptr;
+    Index front_;
+    Index back_;
+    size_t size_;
+    size_t arr_size_;
+public:
+    using value_type = T;
+
     my_deque() = default;
+    void push_back(const T& val);
+
+    ~my_deque();
 };
 
+template <typename T>
+void my_deque<T>::push_back(const T& val) {
+    if(!arr_) {
+        arr_ = new T*[DEFAULT_SIZE];
+        arr_size_ = DEFAULT_SIZE;
+        front_.i = back_.i = 1;
+        front_.j = back_.j = 0;
+        for(size_t i = 0; i < arr_size_;++i){
+            arr_[i] = reinterpret_cast<T*>(new char[SIZE_OF_BUCKET*sizeof(T)]);
+        }
+            
+        new(arr_[back_.i]) T(val); // check for throw next time
 
+    }
+}
+
+template <typename T>
+my_deque<T>::~my_deque() {
+    if(arr_) {
+        for(size_t i = front_.i;i < back_.i;++i) {
+            for(size_t j = front_.j;j < back_.j;++j) {
+                (arr_[i] + j)->~T();
+            }
+        }
+
+        for(size_t i = 0;i < arr_size_;++i){
+            delete[] reinterpret_cast<char*>(arr_[i]);
+        }
+
+        delete[] arr_;
+    }
+}
 int main() {
+    my_deque<int> d;
+    d.push_back(3);
 
     
 }
